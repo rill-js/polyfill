94182c0921abbc07a421b12e1dec84e98960e6a8
Dylan Piercey <pierceydylan@gmail.com>
2016-12-02 19:28:23 -0800

    Update deps

--- test/main.test.js
+++ test/main.test.js
@@ -1,17 +1,13 @@
 'use strict'
 
-var fs = require('fs')
-var path = require('path')
 var test = require('tape')
 var agent = require('supertest').agent
 var rill = require('rill')
 var polyfill = require('../server')
 var MAC_CHROME_UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
-var NORMAL_EXAMPLE = fs.readFileSync(path.join(__dirname, 'normal.polyfill.js'), 'utf8')
-var MINIFIED_EXAMPLE = fs.readFileSync(path.join(__dirname, 'minified.polyfill.js'), 'utf8')
 
 test('Get polyfills from chrome', function (t) {
-  t.plan(2)
+  t.plan(3)
 
   var request = agent(rill()
     .get('/polyfill.js', polyfill({ minify: false }))
@@ -22,15 +18,16 @@ test('Get polyfills from chrome', function (t) {
     .get('/polyfill.js')
     .set('User-Agent', MAC_CHROME_UA)
     .expect(200)
-    .then(function (res) {
-      t.equals(res.text, NORMAL_EXAMPLE.trim(), 'polyfill normal')
-    }, t.fail)
+    .then(function (resNormal) {
+      t.equals(resNormal.headers['content-type'], 'application/javascript', 'polyfill normal')
 
-  request
-    .get('/polyfill.js.min')
-    .set('User-Agent', MAC_CHROME_UA)
-    .expect(200)
-    .then(function (res) {
-      t.equals(res.text, MINIFIED_EXAMPLE.trim(), 'polyfill minified')
+      request
+        .get('/polyfill.js.min')
+        .set('User-Agent', MAC_CHROME_UA)
+        .expect(200)
+        .then(function (resMin) {
+          t.equals(resMin.headers['content-type'], 'application/javascript', 'polyfill minified')
+          t.ok(resNormal.text.length > resMin.text.length, 'should be shorter')
+        }, t.fail)
     }, t.fail)
 })
